513cd3414389116fb05935523be95b99e526395a
Roman Yarlykov <roman.yarlykov@concero.io>
2025-06-23 13:57:59 +0500

    add: PauseDummy

---
+++ deploy/PauseDummy.ts
@@ -0,0 +1,37 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+import { getNetworkEnvKey } from "@concero/contract-utils";
+
+import { conceroNetworks } from "../constants";
+import { log, updateEnvVariable } from "../utils";
+
+const deployPauseDummy: (hre: HardhatRuntimeEnvironment) => Promise<void> = async function (
+	hre: HardhatRuntimeEnvironment,
+) {
+	const { deployer } = await hre.getNamedAccounts();
+	const { deploy } = hre.deployments;
+	const { name, live } = hre.network;
+	const networkType = conceroNetworks[name].type;
+
+	const deployment = await deploy("PauseDummy", {
+		from: deployer,
+		args: [],
+		log: true,
+		autoMine: true,
+	});
+
+	if (live) {
+		log(`Deployed at: ${deployment.address}`, "deployPauseDummy", name);
+		updateEnvVariable(
+			`LANCA_CANONICAL_BRIDGE_PAUSE_${getNetworkEnvKey(name)}`,
+			deployment.address,
+			`deployments.${networkType}`,
+		);
+	}
+
+	return deployment;
+};
+
+deployPauseDummy.tags = ["PauseDummy"];
+
+export default deployPauseDummy;
---
+++ tasks/pause/deployConceroPauseToAllChains.ts
@@ -0,0 +1,23 @@
+import { execSync } from "child_process";
+
+import { task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+import { getNetworkEnvKey } from "@concero/contract-utils";
+
+import { testnetNetworks } from "../../constants/conceroNetworks";
+import { getEnvVar } from "../../utils";
+
+task("deploy-lanca-canonical-bridge-pause-to-all-chains", "").setAction(
+	async (taskArgs, hre: HardhatRuntimeEnvironment) => {
+		for (const chain in testnetNetworks) {
+			if (!getEnvVar(`LANCA_CANONICAL_BRIDGE_PAUSE_${getNetworkEnvKey(chain)}`)) {
+				execSync(`yarn hardhat deploy --tags PauseDummy --network ${chain}`, {
+					stdio: "inherit",
+				});
+			}
+		}
+	},
+);
+
+export default {};

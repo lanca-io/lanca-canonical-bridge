009d549d00aab4e79daccda99eec0e5ea4354ffc
Roman Yarlykov <roman.yarlykov@concero.io>
2025-06-24 14:58:59 +0500

    add: configure FIatToken script

--- tasks/utils/index.ts
+++ tasks/utils/index.ts
@@ -1,3 +1,4 @@
 export { upgradeLancaProxyImplementation } from "./upgradeLancaProxy";
 export { initializeDefaultFiatToken } from "./initializeDefaultFiatToken";
 export { initializeFiatToken } from "./initializeFiatToken";
+export { configureFiatToken } from "./configureFiatToken";

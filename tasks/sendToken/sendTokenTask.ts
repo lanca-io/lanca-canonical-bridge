5e90413ff00375fc6ad7ecf0e528304fb8520345
Roman Yarlykov <roman.yarlykov@concero.io>
2025-06-24 20:22:25 +0500

    fix: sendTokenTask test

--- tasks/sendToken/sendTokenTask.ts
+++ tasks/sendToken/sendTokenTask.ts
@@ -4,19 +4,19 @@ import { type HardhatRuntimeEnvironment } from "hardhat/types";
 import { sendToken } from "../utils/sendToken";
 
 async function sendTokenTask(taskArgs: any, hre: HardhatRuntimeEnvironment) {
-	const { dstChain, amount, gasLimit } = taskArgs;
+	const { dstchain, amount, gaslimit } = taskArgs;
 
 	await sendToken(hre, {
-		dstChain,
+		dstChain: dstchain,
 		amount,
-		gasLimit,
+		gasLimit: gaslimit,
 	});
 }
 
 task("send-token", "Send tokens from one network to another via bridge")
-	.addParam("dstChain", "Destination network name (e.g., 'arbitrumSepolia', 'baseSepolia')")
+	.addParam("dstchain", "Destination network name (e.g., 'arbitrumSepolia', 'baseSepolia')")
 	.addParam("amount", "Amount of USDC to send (e.g., '10.5')")
-	.addParam("gasLimit", "Gas limit for destination transaction (e.g., '200000')")
+	.addParam("gaslimit", "Gas limit for destination transaction (e.g., '200000')")
 	.setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
 		await sendTokenTask(taskArgs, hre);
 	});

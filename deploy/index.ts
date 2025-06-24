14b0657d0be862eae0e53a660e6b60d184876ce7
Roman Yarlykov <roman.yarlykov@concero.io>
2025-06-24 13:45:44 +0500

    fix: index

--- deploy/index.ts
+++ deploy/index.ts
@@ -1,4 +1,8 @@
 export { deployLancaCanonicalBridge } from "./LancaCanonicalBridge";
 export { deployLancaCanonicalBridgeProxy } from "./LancaCanonicalBridgeProxy";
 export { deployLancaCanonicalBridgeProxyAdmin } from "./LancaCanonicalBridgeProxyAdmin";
+export { deployFiatToken } from "./FiatToken";
+export { deployFiatTokenProxy } from "./FiatTokenProxy";
+export { deployFiatTokenProxyAdmin } from "./FiatTokenProxyAdmin";
+
 export { default as deployPauseDummy } from "./PauseDummy";

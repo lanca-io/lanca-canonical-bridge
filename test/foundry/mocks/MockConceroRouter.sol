6fa6bc21268840f20d4197ed3dc20645e577e747
Roman Yarlykov <roman.yarlykov@concero.io>
2025-06-30 20:32:37 +0500

    test: refactor

---
+++ test/foundry/mocks/MockConceroRouter.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+/**
+ * @title Security Reporting
+ * @notice If you discover any security vulnerabilities, please report them responsibly.
+ * @contact email: security@concero.io
+ */
+pragma solidity 0.8.28;
+
+import {ConceroTypes} from "@concero/messaging-contracts-v2/contracts/ConceroClient/ConceroTypes.sol";
+import {IConceroRouter} from "@concero/messaging-contracts-v2/contracts/interfaces/IConceroRouter.sol";
+
+contract MockConceroRouter is IConceroRouter {
+    uint256 public constant MESSAGE_FEE = 100;
+
+    function conceroSend(
+        uint24 /* dstChainSelector */,
+        bool /* shouldFinaliseSrc */,
+        address /* feeToken */,
+        ConceroTypes.EvmDstChainData memory /* dstChainData */,
+        bytes calldata /* message */
+    ) external payable returns (bytes32 messageId) {
+        return bytes32(uint256(1));
+    }
+
+    function getMessageFee(
+        uint24 /* dstChainSelector */,
+        bool /* shouldFinaliseSrc */,
+        address /* feeToken */,
+        ConceroTypes.EvmDstChainData memory /* dstChainData */
+    ) external pure returns (uint256) {
+        return MESSAGE_FEE;
+    }
+}
--- test/foundry/scripts/deploy/DeployMockConceroRouter.s.sol
+++ test/foundry/scripts/deploy/DeployMockConceroRouter.s.sol
@@ -8,31 +8,7 @@ pragma solidity 0.8.28;
 
 import {Script} from "forge-std/src/Script.sol";
 
-import {ConceroTypes} from "@concero/messaging-contracts-v2/contracts/ConceroClient/ConceroTypes.sol";
-import {IConceroRouter} from "@concero/messaging-contracts-v2/contracts/interfaces/IConceroRouter.sol";
-
-contract MockConceroRouter is IConceroRouter {
-    uint256 public constant MESSAGE_FEE = 100;
-
-    function conceroSend(
-        uint24 /* dstChainSelector */,
-        bool /* shouldFinaliseSrc */,
-        address /* feeToken */,
-        ConceroTypes.EvmDstChainData memory /* dstChainData */,
-        bytes calldata /* message */
-    ) external payable returns (bytes32 messageId) {
-        return bytes32(uint256(1));
-    }
-
-    function getMessageFee(
-        uint24 /* dstChainSelector */,
-        bool /* shouldFinaliseSrc */,
-        address /* feeToken */,
-        ConceroTypes.EvmDstChainData memory /* dstChainData */
-    ) external pure returns (uint256) {
-        return MESSAGE_FEE;
-    }
-}
+import {MockConceroRouter} from "../../mocks/MockConceroRouter.sol";
 
 contract DeployMockConceroRouter is Script {
     function deployConceroRouter() public returns (MockConceroRouter) {

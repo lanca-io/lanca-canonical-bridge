bc8f68179007905d0567d5386412ac348d7ee804
Roman Yarlykov <roman.yarlykov@concero.io>
2025-06-30 20:30:10 +0500

    test: add pool tests

--- test/foundry/LCBridgePool/LancaCanonicalBridgePool.t.sol
+++ test/foundry/LCBridgePool/LancaCanonicalBridgePool.t.sol
@@ -6,6 +6,61 @@
  */
 pragma solidity 0.8.28;
 
-contract LancaCanonicalBridgePoolTest {
-    function setUp() public {}
+import {CommonErrors} from "@concero/messaging-contracts-v2/contracts/common/CommonErrors.sol";
+import {LCBridgePoolTest} from "./base/LCBridgePoolTest.sol";
+import {MockUSDC} from "../mocks/MockUSDC.sol";
+
+contract LancaCanonicalBridgePoolTest is LCBridgePoolTest {
+    function setUp() public override {
+        super.setUp();
+    }
+
+    function test_deposit_RevertsUnauthorized() public {
+        vm.expectRevert(CommonErrors.Unauthorized.selector);
+
+        vm.prank(user);
+        lancaCanonicalBridgePool.deposit(user, AMOUNT);
+    }
+
+    function test_deposit_Success() public {
+        _approvePool(AMOUNT);
+
+        vm.prank(deployer);
+        bool success = lancaCanonicalBridgePool.deposit(deployer, AMOUNT);
+
+        assertTrue(success);
+        assertEq(MockUSDC(usdc).balanceOf(address(lancaCanonicalBridgePool)), AMOUNT);
+    }
+
+    function test_withdraw_RevertsUnauthorized() public {
+        vm.expectRevert(CommonErrors.Unauthorized.selector);
+
+        vm.prank(user);
+        lancaCanonicalBridgePool.withdraw(user, AMOUNT);
+    }
+
+    function test_withdraw_Success() public {
+        _approvePool(AMOUNT);
+
+        vm.prank(deployer);
+        lancaCanonicalBridgePool.deposit(deployer, AMOUNT);
+
+        uint256 deployerBalanceBefore = MockUSDC(usdc).balanceOf(deployer);
+
+        vm.prank(deployer);
+        bool success = lancaCanonicalBridgePool.withdraw(deployer, AMOUNT);
+
+        assertEq(MockUSDC(usdc).balanceOf(deployer), deployerBalanceBefore + AMOUNT);
+        assertEq(MockUSDC(usdc).balanceOf(address(lancaCanonicalBridgePool)), 0);
+        assertEq(success, true);
+    }
+
+    function test_getPoolInfo() public {
+        test_deposit_Success();
+
+        (uint24 dstChainSelector, uint256 lockedUSDC) = lancaCanonicalBridgePool.getPoolInfo();
+
+        assertEq(dstChainSelector, DST_CHAIN_SELECTOR);
+        assertEq(lockedUSDC, AMOUNT);
+    }
 }
--- test/foundry/LCBridgePool/base/LCBridgePoolTest.sol
+++ test/foundry/LCBridgePool/base/LCBridgePoolTest.sol
@@ -8,6 +8,7 @@ pragma solidity 0.8.28;
 
 import {LancaCanonicalBridgePool} from "contracts/LancaCanonicalBridgePool/LancaCanonicalBridgePool.sol";
 
+import {MockUSDC} from "../../mocks/MockUSDC.sol";
 import {BridgeTest} from "../../utils/BridgeTest.sol";
 import {DeployLCBridgePool} from "../../scripts/deploy/DeployLCBridgePool.s.sol";
 
@@ -15,6 +16,13 @@ abstract contract LCBridgePoolTest is DeployLCBridgePool, BridgeTest {
     function setUp() public virtual override(DeployLCBridgePool, BridgeTest) {
         super.setUp();
 
-        lancaCanonicalBridgePool = LancaCanonicalBridgePool(deploy());
+        lancaCanonicalBridgePool = LancaCanonicalBridgePool(
+            deploy(usdc, deployer, DST_CHAIN_SELECTOR)
+        );
+    }
+
+    function _approvePool(uint256 amount) internal {
+        vm.prank(deployer);
+        MockUSDC(usdc).approve(address(lancaCanonicalBridgePool), amount);
     }
 }
